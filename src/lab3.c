350fa5e97d963bd7a6650d96f1c89b4e6adb8751
scholtes <scholtes.garrett@gmail.com>
2015-09-12 15:02:41 -0400

    Added statistics - lab now 'works', but with all the 'expected issues'

--- src/lab3.c
+++ src/lab3.c
@@ -12,21 +12,35 @@
 #include <linux/module.h>
 #include <linux/kthread.h>
 
-#define ARR_SIZE 1000
+#define ARR_SIZE 1000000
 
 static struct task_struct *t1;
 static struct task_struct *t2;
 static int arr[ARR_SIZE];
 static int idx = 0;
 
+static int stat[ARR_SIZE] = { 0 };
+static int cs1 = 0;
+static int cs2 = 0;
+
 MODULE_LICENSE("GPL");
 
-int incrementer( void *ptr ) {
-    printk(KERN_INFO "Consumer TID %ld\n", (long)ptr);
+int incrementer( void *ptr_void ) {
+    long ptr;
+    ptr = (long)ptr_void;
+
+    printk(KERN_INFO "Consumer TID %ld\n", ptr);
 
     while(idx < ARR_SIZE) {
         arr[idx++] += 1;
         schedule();
+
+        // Do a stats test (described in part 1)
+        if(ptr == 1) {
+            cs1++;
+        } else {
+            cs2++;
+        }
     }
     return 0;
 }
@@ -53,7 +67,24 @@ int init_module( void ) {
 }
 
 void cleanup_module( void ) {
+    int i;
+
     printk("lab3 module uninstalling\n");
 
+    for(i = 0; i < ARR_SIZE; i++) {
+        stat[arr[i]] += 1;
+    }
+
+    for(i = 0; i < ARR_SIZE; i++) {
+        if(stat[i] > 0) {
+            printk("[HIT] stat[%d] = %d\n", i, stat[i]);
+        }
+    }
+
+    printk("cs1 =     %d\n", cs1);
+    printk("cs2 =     %d\n", cs2);
+    printk("cs1+cs2 = %d\n", cs1+cs2);
+
+    printk("lab3 module uninstalled\n");
     return;
 }
