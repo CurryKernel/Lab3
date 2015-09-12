b6bddd7d507b1e5b8fd9e873f04df767d0a73e07
scholtes <scholtes.garrett@gmail.com>
2015-09-12 14:21:19 -0400

    Added basic thread stuff (copy paste pretty much)

--- src/lab3.c
+++ src/lab3.c
@@ -12,10 +12,42 @@
 #include <linux/module.h>
 #include <linux/kthread.h>
 
+#define ARR_SIZE 1000
+
+static struct task_struct *t1;
+static struct task_struct *t2;
+static int arr[ARR_SIZE];
+static int idx = 0;
+
 MODULE_LICENSE("GPL");
 
+int incrementer( void *ptr ) {
+    printk(KERN_INFO "Consumer TID %ld\n", (long)ptr);
+
+    while(idx < ARR_SIZE) {
+        arr[idx++] += 1;
+        schedule();
+    }
+    return 0;
+}
+
 int init_module( void ) {
+    long id1;
+    long id2;
+
     printk("lab3 module installing\n");
+
+    id1 = 1;
+    id2 = 2;
+    t1 = kthread_create(incrementer, (void*)id1, "inc1");
+    t2 = kthread_create(incrementer, (void*)id2, "inc2");
+    if(t1 && t2) {
+        printk(KERN_INFO "Starting...\n");
+        wake_up_process(t1);
+        wake_up_process(t2);
+    } else {
+        printk(KERN_EMERG "Error\n");
+    }
 
     return 0;
 }
